37be5c1c8cf3bdbb507b6dea5f8e1b886910794d
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-06 11:44:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="Appliction Name", page_icon = ":bar_chart:",layout="wide", initial_sidebar_state="auto", menu_items=None)
-st.title('<Business Name> Performane Dashboard')
+st.title('Application Name')
 st.write('--------')
